69b5b56d2e68abdbf902675bb8f8213c128d186f
Sherif Cherfa <69639595+cherfa11209@users.noreply.github.com>
2021-02-24 16:19:27 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,113 +1,73 @@
-let n = 8;
-let board = Array.from(Array(n), ()=> new Array("x", "x", "x", "x", "x", "x", "x", "x"));
-let counter = 1;
-let knight_moves = [[1, 2], [2, 1], [-1, 2], [2, -1], [-2, 1], [1, -2], [-2, -1], [-1, -2]]
-let row = 0;
-let col = 0;
-let xrow = "";
-let xcol = "";
+let n = 5;
+let board = Array.from(Array(n), ()=> new Array(0, 0, 0, 0, 0));
+let x_moves = [2, 1, 2, -1, -2, 1, -2, -1]
+let y_moves = [1, 2, -1, 2, 1, -2, -1, -2]
+let move_count = 0;
 
-function display(){
+
+//checks if board is complete, returns false if board still has unoccupied cells (working)
+
+const check_if_board_is_complete = () => {
     board.forEach((row) => {
-        console.log(row)
-        console.log("--------------------------------")
-    })
-}
-
-
-display()
-
-
-
-function place_knight(row, col){
-    board[row][col] = counter;
-    counter++;
-}
-
-
-
-function find_availability(row, col){
-    if(board[row][col] == "x"){
-        return true
-    } else if(board[row][col] != "x"){
-        return false
-    }
-}
-
-
-
-
-function find_target_cells(row, col){
-
-    legal_moves = []
-    knight_moves.forEach((move) =>{
-       if((row + move[0]) >= 0 && (row + move[0]) < n && (col + move[1]) >= 0 && (col + move[1]) < n && find_availability((row + move[0]), (col, move[1])) == true){
-           legal_moves.push([(row + move[0]), (col + move[1])])
-       }
-    })
-    console.log(`your legal moves are ${legal_moves}`)
-    return legal_moves
-}
-
-
-
-
-function determine_ideal_cell(array){
-    let accessibility = []
-    let ideal = [];
-
-    array.forEach((arr) =>{
-       let num = (find_target_cells(arr[0], arr[1])).length 
-       if (num < accessibility[0] || accessibility[0] == undefined){
-           accessibility[0] = num
-           ideal = [arr[0], arr[1]]
-           console.log(`The accessibility number is ${accessibility} for the cell, ${ideal}`)
-       } else {
-           console.log('skipping')
-       }
-    })
-
-    console.log(ideal)
-    row = ideal[0];
-    col = ideal[1];
-    console.log(row, col)
-
-    if(find_availability(row, col) == true ){
-        board[row][col] = counter;
-        counter++;
-    }
-
-    
-}
-
-function check_board(){
-    empty_cells = []
-    board.forEach((row) => {
-        row.forEach((cell) =>{
-            if(cell == undefined){
-                empty_cells.push(cell)
-            } else {
-                console.log("")
+        row.forEach((cell) => {
+            if(cell < 0){
+                return false 
             }
         })
     })
+} 
 
-    if(empty_cells.length > 0){
-        return false
-    } else {
-        return false
-    }
+//Displays board using forEach (working)
+
+const display_board = () => {
+    board.forEach((row) => {
+        console.log(row)
+        console.log("--------------------------------------")
+    })
 }
 
+//finds warnsdorff value for a single cell(working)
 
+const find_cell_accessibility = (row, col) => {
 
+    let accessible_cells = []
 
-function solve_board(row, col){
-    for(i = counter; i < 64; i++){
-    place_knight(row, col)
-    determine_ideal_cell(find_target_cells(row, col))
-    display()
+    for(i = 0; i < x_moves.length; i++){
+        let new_row = row + x_moves[i]
+        let new_col = col + y_moves[i]
+
+        if(in_bounds(new_row, new_col)){
+            accessible_cells.push([new_row, new_col])
+        }
     }
+
+    board[row][col] = (- (accessible_cells.length))
 }
 
-solve_board(row, col)
+//this function checks if move is legal (working)
+
+const in_bounds = (row, col) => {
+    if(row >= 0 && row < n && col >= 0 && col < n && board[row][col] < 1){
+        console.log('working')
+        return true
+    }   
+}
+
+//this functions goes on every cell in our board and calculates the it's accessibility range then displays it on the board()
+
+const find_cells_Warnsdorff_values = () => {
+
+    let x = [0, 1, 2, 3, 4]
+    let y = [0, 1, 2, 3, 4]
+
+    for(k = 0; k < x.length; k++){
+        for(j = 0; j <y.length; j++){
+            find_cell_accessibility(x[k], y[j])
+        }
+    }
+
+}
+
+find_cell_accessibility(0, 0)
+find_cells_Warnsdorff_values()
+display_board()
